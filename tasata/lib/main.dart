e9e21b947f92e2f0efbeafbd9b33a8e21955c9a9
salimo2002 <89202366+salimo2002@users.noreply.github.com>
2024-11-14 23:10:29 +0300

    aa

--- tasata/lib/main.dart
+++ tasata/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
 //taha_fakhoury
 //salimo
-//taha
+//tahakkkkkkkkkkkkkkkk
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
