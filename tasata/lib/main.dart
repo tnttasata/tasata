dd78fb5cd5e9696803f419275415a3693586fb3d
tnttasata <tnttasata@gmail.com>
2024-11-14 23:08:00 +0300

    taassw

--- tasata/lib/main.dart
+++ tasata/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
 //taha_fakhoury
 //salimo
+//taha
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
