12ff81be5f29a40bf0c7c858f9de7f08a0e71528
tnttasata <tnttasata@gmail.com>
2024-11-14 22:48:05 +0300

    taha_f

--- tasata/lib/main.dart
+++ tasata/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-//czddsdw
+//czddsdwsjdgjk'lsgdjgosjfskghsdjgksdjgjfs'l
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
