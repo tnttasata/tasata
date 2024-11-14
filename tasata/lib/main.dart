28446b17d74d56269f808ee64f645f37bcd04b09
tnttasata <tnttasata@gmail.com>
2024-11-14 23:01:59 +0300

    salimo

--- tasata/lib/main.dart
+++ tasata/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
 //taha_fakhoury
+//salimo
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
