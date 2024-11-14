1f481aeabebac7a817cf40278a85e3a4ecface19
tnttasata <tnttasata@gmail.com>
2024-11-14 23:14:09 +0300

    salimo see

--- tasata/lib/main.dart
+++ tasata/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
 //taha_fakhoury
 //salimo
 //tahakkkkkkkkkkkkkkkk
-//salimo
+//salimo love you
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
