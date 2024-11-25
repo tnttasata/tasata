e28aa8b2c9308c1311da10f93329191ec1d695df
taha <asdfghmnt@gmail.com>
2024-11-25 18:17:56 +0300

    re

--- tasata/lib/main.dart
+++ tasata/lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:tasata/homa_view.dart';
+import 'package:tasata/views/homa_view.dart';
 
 void main(List<String> args) {
   runApp(const Albayan());
 }
+
 class Albayan extends StatelessWidget {
   const Albayan({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(//sssssssssss
-      home: Scaffold(body:HomeView()),
+    return const MaterialApp(
+      home: Scaffold(body: HomeView()),
     );
   }
 }
